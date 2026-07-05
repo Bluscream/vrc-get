eb766cec9807e218444d71cce2542054827d37bf
anatawa12 <anatawa12@icloud.com>
2026-07-06 00:24:02 +0900

    feat: create online installer for booth distribution

--- vrc-get-gui/src/updater.rs
+++ vrc-get-gui/src/updater.rs
@@ -13,28 +13,21 @@
 //!
 //! This is based heavily on the tauri-plugin-updater source code.
 
-use std::collections::HashMap;
+pub mod common;
+
 use std::ffi::OsString;
 use std::io::Cursor;
 use std::path::{Path, PathBuf};
-use std::str::FromStr;
 
-use base64::Engine as _;
+use crate::updater::common::{
+    PUBLIC_KEY, ReleaseManifestPlatform, RemoteRelease, VerifySignatureError,
+};
+use common::verify_signature;
 use futures::StreamExt as _;
-use minisign_verify::{PublicKey, Signature};
 use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::{StatusCode, header};
-use semver::Version;
-use serde::{Deserialize, Deserializer, de::Error as DeError};
 use tauri::{AppHandle, Env, Manager as _, Runtime};
 use url::Url;
-
-// ---------------------------------------------------------------------------
-// constants
-// ---------------------------------------------------------------------------
-
-static PUBLIC_KEY: &str = "dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IDkyMjAzMkU2Q0ZGQjQ0MjYKUldRbVJQdlA1aklna2d2NnRoM3ZsT3lzWEQ3MC9zTGpaWVR4NGdQOXR0UGJaOHBlY2xCcFY5bHcK";
-
 // ---------------------------------------------------------------------------
 // Error type
 // ---------------------------------------------------------------------------
@@ -109,91 +102,6 @@ impl From<url::ParseError> for Error {
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
-
-// ---------------------------------------------------------------------------
-// Signature verification
-// ---------------------------------------------------------------------------
-
-fn verify_signature(data: &[u8], release_signature: &str, pub_key: &str) -> Result<bool> {
-    if std::env::var(
-        "___ALCOM_UPDATER_DISABLE_SIGNATURE_VERIFICATION_DEBUG_ONLY_FEATURE_DO_NOT_USE_THIS_OR_YOU_WILL_BE_HACKED___",
-    )
-    .as_deref()
-        == Ok("YES_I_WANT_TO_BE_HACKED")
-    {
-        return Ok(true);
-    }
-    let pub_key_decoded = base64_to_string(pub_key)?;
-    let public_key =
-        PublicKey::decode(&pub_key_decoded).map_err(|e| Error::Signature(e.to_string()))?;
-    let sig_decoded = base64_to_string(release_signature)?;
-    let signature = Signature::decode(&sig_decoded).map_err(|e| Error::Signature(e.to_string()))?;
-    public_key
-        .verify(data, &signature, true)
-        .map_err(|e| Error::Signature(e.to_string()))?;
-    Ok(true)
-}
-
-fn base64_to_string(base64_string: &str) -> Result<String> {
-    let decoded = base64::engine::general_purpose::STANDARD.decode(base64_string)?;
-    std::str::from_utf8(&decoded)
-        .map(|s| s.to_string())
-        .map_err(|_| Error::SignatureUtf8(base64_string.into()))
-}
-
-// ---------------------------------------------------------------------------
-// Remote release structures
-// ---------------------------------------------------------------------------
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct ReleaseManifestPlatform {
-    pub url: Url,
-    pub signature: String,
-    // alcom specific information
-    /// Command line parameters for windows installer
-    ///
-    /// If one of arg is prefixed with '!', such parameters have special handling.
-    /// If the updater cannot process ! args, such parameters will be ignored.
-    ///
-    /// Current ! operations are shown below:
-    /// - `!peruser:` appended only if t installation is user installuser install is active
-    /// - `!current installation is machine install
-    // /// - `!install-path:` substitute `${installed}` with currently installed dir. // initially planned but not implemented.
-    #[serde(default)]
-    pub args: Vec<String>,
-}
-
-#[derive(Deserialize)]
-struct RemoteRelease {
-    #[serde(alias = "name", deserialize_with = "parse_version")]
-    version: Version,
-    notes: Option<String>,
-    platforms: HashMap<String, ReleaseManifestPlatform>,
-}
-
-fn parse_version<'de, D>(deserializer: D) -> std::result::Result<Version, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct Visitor;
-
-    impl<'de> serde::de::Visitor<'de> for Visitor {
-        type Value = Version;
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            formatter.write_str("a semver version")
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Version::from_str(v.trim_start_matches('v'))
-                .map_err(|_| DeError::invalid_value(serde::de::Unexpected::Str(v), &self))
-        }
-    }
-
-    deserializer.deserialize_str(Visitor)
-}
 
 // ---------------------------------------------------------------------------
 // OS / arch helpers  (always compiled for all targets – use cfg!())
@@ -532,7 +440,13 @@ impl UpdaterInformation {
             buffer.extend_from_slice(&chunk);
         }
 
-        verify_signature(&buffer, &self.platform.signature, PUBLIC_KEY)?;
+        verify_signature(&buffer, &self.platform.signature, PUBLIC_KEY).map_err(|e| match e {
+            VerifySignatureError::InvalidBase64(e) => Error::InvalidBase64(e),
+            VerifySignatureError::MiniSignError(e) => Error::Signature(e.to_string()),
+            VerifySignatureError::SignatureIsNotUtf8 => {
+                Error::SignatureUtf8(self.platform.signature.clone())
+            }
+        })?;
 
         Ok(buffer)
     }
