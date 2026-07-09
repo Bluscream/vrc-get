bc8c8020e7af4028779ac46d96af504c62d75985
anatawa12 <anatawa12@icloud.com>
2026-07-09 10:02:22 +0900

    chore: improve useCallbackEvent with use of useEffect

--- vrc-get-gui/lib/use-callback-event.ts
+++ vrc-get-gui/lib/use-callback-event.ts
@@ -1,4 +1,15 @@
-import { useCallback, useRef } from "react";
+import { createContext, use, useCallback, useEffect, useRef } from "react";
+
+const dummy = createContext(null);
+
+function isInRender() {
+	try {
+		use(dummy);
+		return true;
+	} catch {
+		return false;
+	}
+}
 
 /**
  * Something like useEffectEvent
@@ -8,9 +19,15 @@ export function useCallbackEvent<Args extends unknown[]>(
 	listener: (...args: Args) => void,
 ): (...args: Args) => void {
 	const event = useRef<(...args: Args) => void>(listener);
-	event.current = listener;
+
+	useEffect(() => {
+		event.current = listener;
+	}, [listener]);
 
 	return useCallback((...args: Args) => {
+		if (isInRender()) {
+			throw new Error("Cannot call an event handler while rendering.");
+		}
 		event.current(...args);
 	}, []);
 }
